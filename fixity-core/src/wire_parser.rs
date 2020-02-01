81c7c843fb5b39747504b7e5a4249f56cb43c2b4
Bradlee Speice <bradlee@speice.io>
2020-02-01 13:36:16 -0500

    Rename to `RawTag`

--- fixity-core/src/wire_parser.rs
+++ fixity-core/src/wire_parser.rs
@@ -10,7 +10,7 @@ const SOH: u8 = 0x01;
 
 /// Base FIX protocol tag/value pair
 #[derive(Debug, PartialEq)]
-pub struct Tag<'a> {
+pub struct RawTag<'a> {
     /// Tag number for a FIX message
     pub tag: u16,
     /// Unparsed data associated with a specific tag
@@ -47,12 +47,12 @@ where
 /// and value bytes.
 ///
 /// ```rust
-/// # use fixity_core::wire_parser::{Tag, delimited};
+/// # use fixity_core::wire_parser::{RawTag, delimited};
 /// let message = "8=FIX.4.4|".as_bytes();
 /// let (_, tag) = delimited(b'|')(message).unwrap();
-/// assert_eq!(tag, Tag { tag: 8, value: "FIX.4.4".as_bytes() });
+/// assert_eq!(tag, RawTag { tag: 8, value: "FIX.4.4".as_bytes() });
 /// ```
-pub fn delimited(delimiter: u8) -> impl Fn(&[u8]) -> IResult<&[u8], Tag> {
+pub fn delimited(delimiter: u8) -> impl Fn(&[u8]) -> IResult<&[u8], RawTag> {
     move |i: &[u8]| {
         let (rem, (tag_bytes, _, value, _)) = tuple((
             take_till1(|b| !is_digit(b)),
@@ -63,7 +63,7 @@ pub fn delimited(delimiter: u8) -> impl Fn(&[u8]) -> IResult<&[u8], Tag> {
 
         Ok((
             rem,
-            Tag {
+            RawTag {
                 tag: atoi(tag_bytes),
                 value,
             },
@@ -75,12 +75,12 @@ pub fn delimited(delimiter: u8) -> impl Fn(&[u8]) -> IResult<&[u8], Tag> {
 /// the tag number and value bytes.
 ///
 /// ```rust
-/// # use fixity_core::wire_parser::{Tag, tag};
+/// # use fixity_core::wire_parser::{RawTag, tag};
 /// let message = &[56, 61, 70, 73, 88, 46, 52, 46, 52, 0x01];
 /// let (_, tag) = tag()(message).unwrap();
-/// assert_eq!(tag, Tag { tag: 8, value: "FIX.4.4".as_bytes() });
+/// assert_eq!(tag, RawTag { tag: 8, value: "FIX.4.4".as_bytes() });
 /// ```
-pub fn tag() -> impl Fn(&[u8]) -> IResult<&[u8], Tag> {
+pub fn tag() -> impl Fn(&[u8]) -> IResult<&[u8], RawTag> {
     delimited(SOH)
 }
 
@@ -91,18 +91,18 @@ pub fn tag() -> impl Fn(&[u8]) -> IResult<&[u8], Tag> {
 /// because data tags are allowed to contain the delimiter field within them.
 ///
 /// ```rust
-/// # use fixity_core::wire_parser::{Tag, data_delimited, delimited};
+/// # use fixity_core::wire_parser::{RawTag, data_delimited, delimited};
 /// let message = "24=3|25=||||".as_bytes();
 /// let (rem, len_tag) = delimited(b'|')(message).unwrap();
 ///
-/// assert_eq!(len_tag, Tag { tag: 24, value: b"3" });
+/// assert_eq!(len_tag, RawTag { tag: 24, value: b"3" });
 /// let len_str = std::str::from_utf8(len_tag.value).unwrap();
 /// let len: usize = len_str.parse().unwrap();
 ///
 /// let (rem, data_tag) = data_delimited(len, b'|')(rem).unwrap();
-/// assert_eq!(data_tag, Tag { tag: 25, value: b"|||" });
+/// assert_eq!(data_tag, RawTag { tag: 25, value: b"|||" });
 /// ```
-pub fn data_delimited(count: usize, delimiter: u8) -> impl Fn(&[u8]) -> IResult<&[u8], Tag> {
+pub fn data_delimited(count: usize, delimiter: u8) -> impl Fn(&[u8]) -> IResult<&[u8], RawTag> {
     move |i: &[u8]| {
         let (rem, (tag_bytes, _, value, _)) = tuple((
             take_till1(|b| !is_digit(b)),
@@ -113,7 +113,7 @@ pub fn data_delimited(count: usize, delimiter: u8) -> impl Fn(&[u8]) -> IResult<
 
         Ok((
             rem,
-            Tag {
+            RawTag {
                 tag: atoi(tag_bytes),
                 value
             }
@@ -124,18 +124,18 @@ pub fn data_delimited(count: usize, delimiter: u8) -> impl Fn(&[u8]) -> IResult<
 /// Read a FIX fixed-length data tag delimited by the default (ASCII SOH) delimiter.
 ///
 /// ```rust
-/// # use fixity_core::wire_parser::{Tag, data_tag, tag};
+/// # use fixity_core::wire_parser::{RawTag, data_tag, tag};
 /// let message = &[50, 52, 61, 51, 01, 50, 53, 61, 01, 01, 01, 01];
 /// let (rem, len_tag) = tag()(message).unwrap();
 ///
-/// assert_eq!(len_tag, Tag { tag: 24, value: b"3" });
+/// assert_eq!(len_tag, RawTag { tag: 24, value: b"3" });
 /// let len_str = std::str::from_utf8(len_tag.value).unwrap();
 /// let len: usize = len_str.parse().unwrap();
 ///
 /// let (rem, data_tag) = data_tag(len)(rem).unwrap();
-/// assert_eq!(data_tag, Tag { tag: 25, value: &[01, 01, 01] });
+/// assert_eq!(data_tag, RawTag { tag: 25, value: &[01, 01, 01] });
 /// ```
-pub fn data_tag(length: usize) -> impl Fn(&[u8]) -> IResult<&[u8], Tag> {
+pub fn data_tag(length: usize) -> impl Fn(&[u8]) -> IResult<&[u8], RawTag> {
     data_delimited(length, SOH)
 }
 
